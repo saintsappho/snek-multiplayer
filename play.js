75dba7c7300c00a8dcf900070e9c488ddea14204
saintsappho <robinrosefleur@gmail.com>
2023-11-22 14:39:41 -0700

    console.logs added to play

--- play.js
+++ play.js
@@ -7,5 +7,30 @@ const { UserInterface } = require('./src/UserInterface')
 const { RemoteInterface } = require('./src/RemoteInterface')
 const game = new Game(new UserInterface(), new RemoteInterface())
 
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+  conn.on('connect', () => {
+    console.log('U r snek now')
+  })
+  conn.on('data', (data) => {
+    console.log(data)
+  })
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+
+
 // Begin game
 game.start()
