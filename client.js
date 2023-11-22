f0b4c1b9ac13c5bc34e5751a6a7d2df80407a0fd
saintsappho <robinrosefleur@gmail.com>
2023-11-22 15:01:51 -0700

    name and module

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+  conn.on('connect', () => {
+    console.log('U r snek now')
+    conn.write("Name: WOB")
+  })
+  conn.on('data', (data) => {
+    console.log(data)
+  })
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+module.exports = connect
--- play.js
+++ play.js
@@ -10,22 +10,7 @@ const game = new Game(new UserInterface(), new RemoteInterface())
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
-  });
-  conn.on('connect', () => {
-    console.log('U r snek now')
-  })
-  conn.on('data', (data) => {
-    console.log(data)
-  })
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const connect = require('./client')
 
 console.log("Connecting ...");
 connect();
